4b6492cf04e6137c80594da3b25befabfed5c703
Mørke Drevos <drtseo@proton.me>
2025-11-16 22:20:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -787,7 +787,7 @@ const snapshotDateLabel = snapshotTakenAt
   {isLive
     ? 'WINDOW CLOSES IN'
     : isClosed
-    ? 'NEXT ROUND COMING UP SOON'
+    ? 'NEXT ROUND SOON'
     : 'NEXT WINDOW IN'}
 </p>
 
