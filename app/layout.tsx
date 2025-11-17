a614e3fd428f8be25391e6ab404184375efacf33
Mørke Drevos <drtseo@proton.me>
2025-11-17 21:50:41 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import type React from "react";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "$CLAIM Portal",
