f8e505e590f5736de076bdebe519658bd9953114
Mørke Drevos <drtseo@proton.me>
2025-11-19 21:13:24 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,1 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  return NextResponse.rewrite(new URL('/dead.html', req.url));
-}
