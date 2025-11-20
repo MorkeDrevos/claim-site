b3adb98bac91c3c3388f8762b9de08c1c92c3de4
Mørke Drevos <drtseo@proton.me>
2025-11-20 17:52:32 +0100

    Create WalletContextProvider.tsx

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React, { useMemo } from 'react';
+import { clusterApiUrl } from '@solana/web3.js';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  BackpackWalletAdapter,
+  LedgerWalletAdapter,
+  BraveWalletAdapter,
+  CoinbaseWalletAdapter,
+  ExodusWalletAdapter,
+  TokenPocketWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+
+import { JupiterWalletAdapter } from './JupiterWalletAdapter';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function WalletContextProvider({ children }: Props) {
+  const endpoint = useMemo(() => clusterApiUrl('mainnet-beta'), []);
+
+  const wallets = useMemo(
+    () => [
+      new JupiterWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new BackpackWalletAdapter(),
+      new SolflareWalletAdapter({ network: 'mainnet-beta' }),
+      new LedgerWalletAdapter(),
+      new BraveWalletAdapter(),
+      new CoinbaseWalletAdapter(),
+      new ExodusWalletAdapter(),
+      new TokenPocketWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect={true}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
