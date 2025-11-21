b78b27800534d0caaf4959db350d5f97210a322f
Mørke Drevos <drtseo@proton.me>
2025-11-21 10:57:15 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1309,7 +1309,7 @@ const activeStep = activeIndex >= 0 ? steps[activeIndex] : null;
               </div>
 
               {/* MOBILE CONNECT CTA */}
-<div className="mt-4 mb-4 block sm:hidden">
+<div className="mt-5 mb-4 block sm:hidden">
   <ConnectWalletButton variant="mobile" />
 </div>
             </div>
