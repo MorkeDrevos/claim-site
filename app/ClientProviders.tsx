40062980d6c7550abcb8e9c0269b4e1dc2c6017b
Mørke Drevos <drtseo@proton.me>
2025-11-21 18:31:44 +0100

    Update ClientProviders.tsx

--- app/ClientProviders.tsx
+++ app/ClientProviders.tsx
@@ -1,19 +1,15 @@
 'use client';
 
 import React from 'react';
-import { Analytics } from '@vercel/analytics/react';
-import '@solana/wallet-adapter-react-ui/styles.css';
-
 import WalletContextProvider from '../components/WalletContextProvider';
 import { ToastProvider } from './Toast';
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <WalletContextProvider>
-      <ToastProvider>
-        {children}
-        <Analytics />
-      </ToastProvider>
+      <ToastProvider>{children}</ToastProvider>
     </WalletContextProvider>
   );
 }
+
+export default ClientProviders;
