0625e45d6c07e05962b5d5bfa907518c405493ff
Mørke Drevos <drtseo@proton.me>
2025-11-21 14:48:56 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react'; 
 import Link from 'next/link';
 import Image from 'next/image';
 import { useToast } from './Toast';
