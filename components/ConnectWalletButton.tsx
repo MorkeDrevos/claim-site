0a0a57494e26a426563f90f56a21a552861d4760
Mørke Drevos <drtseo@proton.me>
2025-11-21 01:15:04 +0100

    Update ConnectWalletButton.tsx

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -1,7 +1,8 @@
-"use client";
+'use client';
 
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export default function ConnectWalletButton() {
-  return <WalletMultiButton />;
+  // We attach our own class so we can style it reliably
+  return <WalletMultiButton className="claim-wallet-btn" />;
 }
